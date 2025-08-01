12d9834f612c31aeaec03b39895e37077599069b
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-08-01 17:06:14 +0100

    Add JePL file structure

--- .sqa/script.tasty-sepia-swan.sh
+++ .sqa/script.tasty-sepia-swan.sh
@@ -7,6 +7,6 @@
 cd github.com/interTwin-eu/itwinai-plugin-template &&
     python -m venv .venv &&&&
     source .venv/bin/activate &&&&
-    .venv/bin/pip install &&&&
+    .venv/bin/pip install . &&&&
     .venv/bin/pytest -v tests/
 )
